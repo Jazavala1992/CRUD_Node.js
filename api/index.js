8f3a9c6912795651640237c4e565526685a25a06
Jose Zavala <jazavala@umsa.bo>
2025-06-25 21:04:53 -0400

    Add test.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,4 @@
-require('dotenv').config();
 
-const mysql = require('mysql2');
-
-// Configuración de base de datos
-let dbConfig;
-
-if (process.env.MYSQL_URL) {
-    const url = new URL(process.env.MYSQL_URL);
-    dbConfig = {
-        host: url.hostname,
-        port: parseInt(url.port),
-        user: url.username,
-        password: url.password,
-        database: url.pathname.substring(1),
-        acquireTimeout: 60000,
-        timeout: 60000,
-        reconnect: true
-    };
-}
 
 // Usar module.exports en lugar de export default
 module.exports = (req, res) => {
